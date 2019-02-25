07cb8c8f27c26e2bd794ce2b5f9007841721fbe9
hongghe <364930855@qq.com>
2019-02-25 14:59:10 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/datajpa/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hongghe.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+}
